26ef11a08dcb18a5b056deaaa2f85ff160025db9
CJEY VILLANUEVA <156678802+Aji-09@users.noreply.github.com>
2025-10-24 03:46:01 +0800

    Fix port assignment in app.py

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ def download_file():
 
     return Response(generate_file(), mimetype='audio/mpeg',
                     headers={"Content-Disposition": f"attachment; filename={mp3_filename}"})
-port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
